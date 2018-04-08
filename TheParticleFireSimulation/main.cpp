2794467792edeabc52cf4d6417d92610ca65aba5
WenXiao <wen.xiao@mail.mcgill.ca>
2018-04-07 23:16:48 -0400

    Add pixels to the screen.

--- TheParticleFireSimulation/main.cpp
+++ TheParticleFireSimulation/main.cpp
@@ -21,14 +21,23 @@ int main() {
         cout << "Error initialising SDL." << endl;
     }
     
-    bool quit = false;
     
-    
-    while(!quit) {
+    while(true) {
         // Update particles
-        // Draw particles
-        // Check for messages/events
         
+        // Draw particles
+        for(int y=0; y < Screen::SCREEN_HEIGHT; y++) {
+            for(int x=0; x < Screen::SCREEN_WIDTH; x++) {
+                screen.setPixel(x, y, 128, 0, 255);
+            }
+        }
+        
+        
+        
+        // Draw the screen
+        screen.update();
+        
+        // Check for messages/events
         if(!screen.processEvents()){
             break;
         }
