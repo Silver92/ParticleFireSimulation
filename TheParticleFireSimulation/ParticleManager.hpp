7ddd24727d3f7ecde61199083b1602849eee62e5
Silver92 <wen.xiao@mail.mcgill.ca>
2019-02-11 11:37:20 -0500

    Added a singleton manager.

--- TheParticleFireSimulation/ParticleManager.hpp
+++ TheParticleFireSimulation/ParticleManager.hpp
@@ -9,6 +9,34 @@
 #ifndef ParticleManager_hpp
 #define ParticleManager_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <math.h>
+#include "Screen.hpp"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "Swarm.hpp"
+
+using namespace std;
+using namespace ParticleFire;
+
+class ParticleManager {
+    Screen screen;
+    Swarm swarm;
+    
+    ParticleManager() {}
+    ParticleManager(ParticleManager const&);
+    void operator=(ParticleManager const&);
+    
+public:
+    static ParticleManager & GetInstance() {
+        static ParticleManager particleManager;
+        return particleManager;
+    }
+    
+    void ScreenInit();
+    void Start();
+};
 
 #endif /* ParticleManager_hpp */
--- TheParticleFireSimulation/main.cpp
+++ TheParticleFireSimulation/main.cpp
@@ -6,65 +6,12 @@
 //  Copyright © 2018 Wen Xiao. All rights reserved.
 //
 
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <math.h>
-#include "Screen.hpp"
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include "Swarm.hpp"
-
-using namespace std;
-using namespace ParticleFire;
+#include "ParticleManager.hpp"
 
 int main() {
     
-    Screen screen;
-    
-    if(screen.init() == false) {
-        cout << "Error initialising SDL." << endl;
-    }
-    
-    Swarm swarm;
-    
-    while(true) {
-        
-        // Update the particle swarms statement
-        int elapsed = SDL_GetTicks();
-        swarm.update(elapsed);
-        
-        // Adjust color changing patterns
-        unsigned char red = (unsigned char)((1 +sin(elapsed * 0.0002)) * 128);
-        unsigned char green = (unsigned char)((1 +sin(elapsed * 0.0001)) * 64);
-        unsigned char blue = (unsigned char)((1 +sin(elapsed * 0.0003)) * 100);
-        
-        const Particle * const pParticles = swarm.getParticles();
-        
-        // Draw Particles
-        for(int i=0; i<Swarm::NPARTICLES; i++) {
-            Particle particle = pParticles[i];
-            
-            int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
-            int y = particle.m_y * Screen::SCREEN_WIDTH / 2 + Screen::SCREEN_HEIGHT/2;
-            
-            screen.setPixel(x, y, red, green, blue);
-        }
-        
-        // Implement the blur effect
-        screen.boxBlur();
-        
-        // Update the screen
-        screen.update();
-        
-        // Check for messages/events
-        if(!screen.processEvents()){
-            break;
-        }
-    }
-    
-    // Post processing after close the screen
-    screen.close();
+    ParticleManager::GetInstance().ScreenInit();
+    ParticleManager::GetInstance().Start();
     
     return 0;
 }
