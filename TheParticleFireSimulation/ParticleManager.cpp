762a2de1b596b190f8fb26169b6cd45df2441150
Silver92 <wen.xiao@mail.mcgill.ca>
2019-02-11 10:48:12 -0500

    Add a manager.

---
+++ TheParticleFireSimulation/ParticleManager.cpp
@@ -0,0 +1,9 @@
+//
+//  ParticleManager.cpp
+//  TheParticleFireSimulation
+//
+//  Created by Silver on 2019-02-11.
+//  Copyright © 2019 Wen Xiao. All rights reserved.
+//
+
+#include "ParticleManager.hpp"
---
+++ TheParticleFireSimulation/ParticleManager.hpp
@@ -0,0 +1,14 @@
+//
+//  ParticleManager.hpp
+//  TheParticleFireSimulation
+//
+//  Created by Silver on 2019-02-11.
+//  Copyright © 2019 Wen Xiao. All rights reserved.
+//
+
+#ifndef ParticleManager_hpp
+#define ParticleManager_hpp
+
+#include <stdio.h>
+
+#endif /* ParticleManager_hpp */
